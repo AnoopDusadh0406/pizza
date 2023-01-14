ae44855a3143c1a1f497d7c95be47a076aee0e1c
anoopdusah <newestjack365@gmail.com>
2023-01-14 16:30:50 +0530

    home page created

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const expressLayout = require('express-ejs-layouts');
 
 const PORT = process.env.PORT || 3000 ;
 
+/// Asset
+
+app.use(express.static('public'));
 app.get('/' , (req,res) =>  {
     res.render('home');
 });
